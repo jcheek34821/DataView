90082b0510b90d6db0aaf8b9f50f83428452c83d
Jared <jared.cheek.2011@gmail.com>
2022-07-26 06:25:20 -0400

    Update

--- DataViewMain.py
+++ DataViewMain.py
@@ -11,11 +11,19 @@ from tkinter import ttk
 from tkinter import filedialog as fd
 from datetime import datetime
 import csv
+
+import numpy as np
 import pyautogui
 import operator
+import PIL
+from PIL import ImageFont
+from PIL import Image
+from PIL import ImageDraw
+from cv2 import CV_32FC1
 
 hard_drive = ""
 path_to_videos = "Yalls 2022/Regular Season(CHOPPED)/"
+path_to_export = "Yalls 2022/Export/"
 global_buttons = []
 display_row = []
 filter_row = []
@@ -41,9 +49,22 @@ ops = {
 
 header_row = []
 ok_button = []
+end_main_process = False
+main_process_in_progress = False
+end_filter_process = False
+filter_process_in_progress = False
+
+
+def text_to_image(text):
+    font = ImageFont.truetype("arial.ttf", 20)
+    img = Image.new("RGBA", (960, 25), (0, 0, 0))
+    draw = ImageDraw.Draw(img)
+    draw.text((0, 0), text, (255, 255, 255), font=font)
+    img.save("C:/Users/Jared/Desktop/img.png")
 
 
 def export_to_mp4():
+    print("Exporting...")
     # frame
     currentframe = 0
     width = 1920
@@ -51,13 +72,13 @@ def export_to_mp4():
 
     mp4string = "new_video.mp4"
     path_count = 1
-    while os.path.exists(hard_drive + path_to_videos + mp4string):
+    while os.path.exists(hard_drive + path_to_export + mp4string):
         mp4string = "new_video(" + str(path_count) + ").mp4"
         path_count += 1
 
     # choose codec according to format needed
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    video = cv2.VideoWriter(hard_drive + path_to_videos + mp4string, fourcc, 30, (width, height))
+    video = cv2.VideoWriter(hard_drive + path_to_export + mp4string, fourcc, 30, (width, height))
 
     x = 0
     while True:
@@ -112,7 +133,7 @@ def play_single_video(video_string, frame_no):
                 r = cv2.resize(pic_fra, (960, 540))
                 cv2.imshow("DataView", r)
                 current_frame = int(vid.get(cv2.CAP_PROP_POS_FRAMES))
-                if cv2.waitKey(10) & 0xFF == ord('q'):
+                if cv2.waitKey(20) & 0xFF == ord('q'):
                     break
             else:
                 cv2.waitKey(0)
@@ -256,10 +277,16 @@ def is_in(this_cell, array):
 def load_filtered_csv():
     global display_row
     global cell
-    remove_rows()
+    global end_filter_process
+    global end_main_process
+    global main_process_in_progress
+    remove_rows(data_frame[0])
     display_row = list(row).copy()
     display_row.remove(display_row[0])
+    main_process_in_progress = True
     for x in filter_row:
+        if end_main_process:
+            break
         n = filter_row.index(x)
         if len(x) > 0:
             if x[0] == "c":
@@ -281,6 +308,8 @@ def load_filtered_csv():
     row_num = 1
     x = 0
     while x < len(display_row):
+        if end_main_process:
+            break
         s = ""
         for y in display_row[x]:
             s += format_text(y)
@@ -290,19 +319,19 @@ def load_filtered_csv():
                     color = "#FFB8B8"
                 else:
                     color = "#FFFFFF"
-                    temp_label = Button(
-                        data_frame[0],
-                        bg=color,
-                        borderwidth=0,
-                        text=s,
-                        width=15 * len(row[0]),
-                        anchor="w",
-                        height=1,
-                        font='courier 10 bold',
-                        command=lambda a=hard_drive, b=path_to_videos,
-                        c=display_row[x][frame_col - 1], d=display_row[x][frame_col]:
-                        play_single_video(str(a + b + c), int(d)))
-                    temp_label.grid(column=0, row=row_num, sticky="news")
+                temp_label = Button(
+                    data_frame[0],
+                    bg=color,
+                    borderwidth=0,
+                    text=s,
+                    width=15 * len(row[0]),
+                    anchor="w",
+                    height=1,
+                    font='courier 10 bold',
+                    command=lambda a=hard_drive, b=path_to_videos,
+                                   c=display_row[x][frame_col - 1], d=display_row[x][frame_col]:
+                    play_single_video(str(a + b + c), int(d)))
+                temp_label.grid(column=0, row=row_num, sticky="news")
             else:
                 if not display_row[x][frame_col].strip():
                     color = "#D89D9D"
@@ -328,16 +357,19 @@ def load_filtered_csv():
             x += 1
         else:
             display_row.remove(display_row[x])
+    end_main_process = False
+    main_process_in_progress = False
     if len(display_row) > 0:
         export.configure(state="normal")
 
 
-def remove_rows():
-    df = data_frame[0].winfo_children()
+def remove_rows(local_container):
+    if local_container == data_frame[0]:
+        export.configure(state="disabled")
+    df = local_container.winfo_children()
     for x in df:
         if df.index(x) > 0:
             x.destroy()
-    export.configure(state="disabled")
 
 
 def size_contained():
@@ -354,12 +386,16 @@ def ok_filter(win, ind):
     global filter_row
     global user_regex
     global global_index_num
+    global end_filter_process
+    global filter_process_in_progress
     user_regex = e1[0].get()
     root.deiconify()
     win.withdraw()
     for x in buffer_array:
         if x[4] == "v":
             if not contains_value(x, filter_row[x[0]]):
+                if len(filter_row[x[0]]) > 0 and filter_row[x[0]][0] == "c":
+                    filter_row[x[0]] = []
                 if x[3] == "a":
                     filter_row[x[0]].append(x[2])
                 else:
@@ -369,6 +405,8 @@ def ok_filter(win, ind):
             global_index_num = condition_options.index(clicked.get())
             filter_row[ind].append("c")
             filter_row[ind].append(x[2])
+            if not e1[0].get() == "":
+                filter_row[ind].append(e1[0].get())
     butt = header_row[ind]
     header = butt["text"][0:len(butt["text"]) - 1]
     if not filter_row[ind] == []:
@@ -377,6 +415,10 @@ def ok_filter(win, ind):
         butt["text"] = header + " \u25BD"
     buffer_array = []
     remove_button.configure(state="disabled")
+    if filter_process_in_progress:
+        end_filter_process = True
+    remove_rows(name_frame)
+    e1[0].delete(0, END)
     clear_name_frame()
     name_frame.update()
     load_filtered_csv()
@@ -385,16 +427,21 @@ def ok_filter(win, ind):
 def cancel_filter(win):
     global buffer_array
     global global_buttons
+    global end_filter_process
+    if filter_process_in_progress:
+        end_filter_process = True
     for x in buffer_array:
         if x[4] == "v":
             global_buttons[x[1]]["text"] = x[2]
     buffer_array = []
     global_buttons = []
+    remove_rows(name_frame)
     clicked.set("None")
     remove_button.config(state="disabled")
     clear_name_frame()
     name_frame.update()
     root.deiconify()
+    e1[0].delete(0, END)
     win.withdraw()
 
 
@@ -403,6 +450,7 @@ def check_value(ind, t_col):
     clicked.set("None")
     enable_widget(ok_button[0])
     ok_button[0]["text"] = "Value OK"
+    e1[0].delete(0, END)
     if "\u2713 " in global_buttons[ind]["text"]:
         global_buttons[ind]["text"] = global_buttons[ind]["text"][2:len(global_buttons[ind]["text"])]
         if not contains_value(global_buttons[ind]["text"], buffer_array):
@@ -449,6 +497,7 @@ def enable_widget(event):
 def clear_filter_row(win, ind):
     global filter_row
     filter_row[ind] = []
+    e1[0].delete(0, END)
     ok_filter(win, ind)
 
 
@@ -462,27 +511,38 @@ def filter_col(regex):
     global user_regex
     global global_index_num
     global global_buttons
-
+    global end_main_process
+    global end_filter_process
+    global filter_process_in_progress
+    if main_process_in_progress:
+        end_main_process = True
     root.withdraw()
     filter_window.deiconify()
 
     value_options = []
     global_index_num = get_index_from_regex(regex)
-    for x in row:
+    for x in display_row:
         if not contains_value(x[global_index_num], value_options):
             value_options.append(x[global_index_num])
-    value_options.remove(regex)
+    # value_options.remove(regex)
 
     if len(filter_row[global_index_num]) > 0:
         remove_button.config(state="normal")
+        if filter_row[global_index_num][0] == "c":
+            e1[0].delete(0, END)
+            e1[0].insert(0, filter_row[global_index_num][2])
 
     # Add options to frame
     row_num = 0
     global_buttons = []
     name_frame.update()
     filter_window.update()
+    filter_process_in_progress = True
     for x in value_options:
-        if len(filter_row[global_index_num]) == 0 or not contains_value(x, filter_row[global_index_num]):
+        if end_filter_process:
+            break
+        if len(filter_row[global_index_num]) == 0 or not contains_value(x, filter_row[global_index_num]) or \
+                filter_row[global_index_num][0] == "c":
             global_buttons.append(Button(
                 name_frame,
                 text=x,
@@ -499,6 +559,8 @@ def filter_col(regex):
         name_frame.update()
         filter_window.update()
         value_canvas.config(scrollregion=value_canvas.bbox("all"))
+    filter_process_in_progress = False
+    end_filter_process = False
 
 
 def format_text(text):
@@ -515,7 +577,7 @@ def load_csv(csv_file):
     global display_row
     global filter_row
     global frame_col
-    remove_rows()
+    remove_rows(data_frame[0])
     display_row.clear()
     row.clear()
     root.update()
@@ -529,6 +591,7 @@ def load_csv(csv_file):
                 col.append(b)
             row.append(col)
             col = []
+        display_row = list(row).copy()
         filter_row = [[] for _ in range(len(row[0]))]
 
     header_frame = Frame(data_frame[0])
@@ -585,7 +648,8 @@ def toggle_video():
 def select_video_drive():
     global hard_drive
     hard_drive = fd.askdirectory(title='Select Hard drive')
-    video_drive["text"] = hard_drive
+    hard_drive = hard_drive[0:3]
+    video_drive["text"] = hard_drive[0:3]
     video_drive.configure(state="disabled")
 
 
@@ -599,14 +663,48 @@ def close_out():
     sys.exit()
 
 
+def select_all():
+    global buffer_array
+    ind = 0
+    buffer_array = []
+    for x in global_buttons:
+        if "\u2713 " not in x["text"]:
+            x["text"] = "\u2713 " + x["text"]
+        buffer_array.append([global_index_num, ind, global_buttons[ind]["text"], "a", "v"])
+        ind += 1
+
+
+def clear_all():
+    global buffer_array
+    buffer_array = []
+    for x in global_buttons:
+        if "\u2713 " in x["text"]:
+            x["text"] = x["text"].replace("\u2713 ", "")
+
+
+def search_buttons(event):
+    global global_buttons
+    reveal_all_global_buttons()
+    remove_rows(name_frame)
+    x = 0
+
+
+def reveal_all_global_buttons():
+    global global_buttons
+    for x in global_buttons:
+        x.place()
+
+
 v_is_toggled = False
 frame_col = 0
 
 screen_width, screen_height = pyautogui.size()
+mod_screen_width = screen_width - 100
+mod_screen_height = int((mod_screen_width - 100) * .5625)
 
 # create the root window
 root = tk.Tk()
-root.geometry("1000x530+" + str(int((screen_width / 2) - 500)) + "+" + str(int((screen_height / 2) - 215)))
+root.geometry(str(mod_screen_width) + "x" + str(mod_screen_height + 35) + "+0+0")
 root.title('DataView')
 root.grid_rowconfigure(0, weight=1)
 root.columnconfigure(0, weight=1)
@@ -614,7 +712,7 @@ root.protocol("WM_DELETE_WINDOW", lambda: close_out())
 
 # create filter window
 filter_window = tk.Tk()
-filter_window.geometry("300x400+" + str(int((screen_width / 2) - 150)) + "+" + str(int((screen_height / 2) - 200)))
+filter_window.geometry("300x500+" + str(int((screen_width / 2) - 150)) + "+" + str(int((screen_height / 2) - 200)))
 filter_window.title('Filter')
 filter_window.protocol("WM_DELETE_WINDOW", disable_event)
 filter_window.withdraw()
@@ -634,16 +732,26 @@ e1.append(Entry(filter_window, bd=1))
 
 condition_label.grid(row=0, column=0, sticky=W)
 o_menu.grid(row=0, column=0, sticky=E)
-e1[-1].grid(row=1, column=0, sticky=W, padx=10)
+e1[-1].grid(row=1, column=0, sticky=W, padx=5)
 
 # Create Dropdown menu
 value_label = Label(filter_window, text="Filter by value: ", font="arial 12")
+value_search_entry = Entry(filter_window, bd=1)
+value_search_entry.bind("<Key>", search_buttons)
+value_select_all = Button(
+    filter_window, borderwidth=0, text="Select All", font="arial 8 underline", command=select_all)
+value_clear_all = Button(
+    filter_window, borderwidth=0, text="Clear", font="arial 8 underline", command=clear_all)
 value_frame = Frame(filter_window, width=290, height=200, highlightbackground="black", highlightthickness=1)
+
 value_frame.grid_rowconfigure(0, weight=1)
 value_frame.grid_columnconfigure(0, weight=1)
 value_frame.grid_propagate(False)
 value_label.grid(row=2, column=0, sticky=W)
-value_frame.grid(row=3, column=0, padx=5)
+value_search_entry.grid(row=3, column=0, padx=5, pady=10, sticky=W)
+value_select_all.grid(row=4, column=0, padx=5, pady=10, sticky=W)
+value_clear_all.grid(row=4, column=0, padx=70, pady=10, sticky=W)
+value_frame.grid(row=5, column=0, padx=5)
 
 value_canvas = tk.Canvas(value_frame, width=290, height=200)
 value_canvas.grid(row=0, column=0, sticky="news")
@@ -664,10 +772,10 @@ ok_button.clear()
 ok_button.append(Button(filter_window, text="OK", command=lambda: ok_filter(filter_window, global_index_num)))
 disable_widget(ok_button[-1])
 cancel = Button(filter_window, text="Cancel", command=lambda: cancel_filter(filter_window))
-remove_button.grid(row=4, column=0, pady=5, sticky="news")
+remove_button.grid(row=6, column=0, pady=5, sticky="news")
 remove_button.config(state="disabled")
-ok_button[-1].grid(row=5, column=0, pady=5, sticky="news")
-cancel.grid(row=6, column=0, pady=5, sticky="news")
+ok_button[-1].grid(row=7, column=0, pady=5, sticky="news")
+cancel.grid(row=8, column=0, pady=5, sticky="news")
 
 row = []
 col = []
@@ -716,14 +824,14 @@ v_toggle = Button(
     command=toggle_video)
 v_toggle.grid(row=0, column=4, sticky=W, padx=10)
 
-container = tk.Frame(window_frame, width=1000, height=500)
+container = tk.Frame(window_frame, width=mod_screen_width, height=mod_screen_height)
 container.grid(row=1, column=0, pady=(5, 0), sticky='nw')
 container.grid_rowconfigure(0, weight=1)
 container.grid_columnconfigure(0, weight=1)
 # Set grid_propagate to False to allow 5-by-5 buttons resizing later
 container.grid_propagate(False)
 
-canvas = tk.Canvas(container, width=1000, height=500)
+canvas = tk.Canvas(container, width=mod_screen_width, height=mod_screen_height)
 canvas.grid(row=0, column=0, sticky="news")
 
 v = tk.Scrollbar(container, orient="vertical", command=canvas.yview)
